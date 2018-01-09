54a933f0c8900d52a36e9f8142e3e2fb4b5d6920
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-01-09 22:35:37 +0000

    add up

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 
-const { schema } = require("./src/Schema.bs.js");
+const { schema } = require("./lib/js/src/schema.bs.js");
 
 const app = express();
 
 const { PORT = 3000, UP_STAGE = "" } = process.env;
-const ENDPOINT_URL = `/`;
 
-// The GraphQL endpoint
-app.post(ENDPOINT_URL, bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.get(
-  ENDPOINT_URL,
-  graphiqlExpress({ endpointURL: `${ENDPOINT_URL}${UP_STAGE}` })
-);
+app.route('/')
+	.get(graphiqlExpress({ endpointURL: `/${UP_STAGE}` }))
+	.post(bodyParser.json(), graphqlExpress({ schema }));
 
 app.listen(PORT);
